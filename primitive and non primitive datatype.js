02b85b723ea23df9385a1e1ab4155bf075cfd0a5
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-17 00:04:21 +0530

    Add files via upload

---
+++ primitive and non primitive datatype.js
@@ -0,0 +1,13 @@
+console.log("var")
+ var b = "gaurav";
+console.log(b);
+{
+  let b ='raju'
+  console.log(b);
+  
+}
+//let b='suar'//
+//console.log(b);//let should not redeclared and changed because it is block scope 
+const author='god'
+console.log(author)
+//author='hg'//not redeclared or change
