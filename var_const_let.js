cf917dcf209d92cfaec505ee8f8659b1780850a5
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-17 00:03:40 +0530

    Add files via upload

---
+++ var_const_let.js
@@ -0,0 +1,18 @@
+//primitive data types 
+let a= null;
+let b=567;
+let c='gaurav';
+let d="$";
+let e=undefined;
+let f=true //false should also
+let g //alwyas undefined
+console.log(a,b,c,d,e,f,g)
+
+//objects in javascript
+const item = {
+  "Gaurav" : true,
+  "Subh" : false,
+  "saurabh": undefined,
+  
+}
+console.log(item["Subh"])
