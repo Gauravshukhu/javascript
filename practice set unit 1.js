9210c535475d77ff611f55f32636a24d1e8a37c0
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-17 00:04:40 +0530

    Add files via upload

---
+++ practice set unit 1.js
@@ -0,0 +1,23 @@
+//practice set 1
+let a ="gaurav";
+let b = 23
+let c= a + b
+console.log(c)
+
+//practice set 2
+console.log(typeof (c))
+
+//pratice set 3
+const d = {
+  name : "gaurav",
+  
+  class: 12,
+  
+  
+}
+console.log(d)
+
+
+
+
+  
