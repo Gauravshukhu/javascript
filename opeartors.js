f34e238c21fd155ed6d990c30b9aa518065e6075
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-18 15:22:11 +0530

    Add files via upload
    its is a javascript bacsic concept on operators.

---
+++ opeartors.js
@@ -0,0 +1,42 @@
+// Aerthematic operators in js
+let a =45
+let b =5
+console.log("a+b =",a+b)
+console.log("a-b =",a-b)
+console.log("a*b =",a*b)
+console.log("a/b =",a/b)
+console.log("a**b =",a**b)
+console.log("a%b =",a%b)
+console.log("a++ =",a++)
+console.log("++a =",++a)
+console.log("--b =",--b)
+console.log("b-- =",b--)
+
+
+//assigment operators in js
+let ass =6
+ass += 4
+console.log(ass)
+ass -=8
+console.log(ass)
+ass *=90
+console.log(ass)
+
+//comparsion operator
+let comp1 = 2
+let comp2 = 4
+console.log(comp1==comp2)
+console.log(comp1!=comp2)
+console.log(comp1>comp2)
+console.log(comp1<comp2)
+console.log(comp1>=comp2)
+console.log(comp1===comp2)//is case mai agar comp1 aur comp2 ki value same hai aur second value ya firsrt value ke andar "6" string mai agar no hai toh answer false ayega .
+
+//LOGICAL operator
+let c=9
+let y=7
+console.log(c>y && c==y)
+
+
+
+
