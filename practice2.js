aee0e96313f30ca345b8a0aa6d0c85f56090c4c8
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-19 12:49:02 +0530

    Add files via upload

---
+++ practice2.js
@@ -0,0 +1,27 @@
+//q1
+
+let age1=prompt("gaurav");
+let age2 =prompt("saurabh");
+age1 =Number.parseInt(age1)
+age2 =Number.parseInt(age2)
+ if (age1>10  && age2<20 ){
+  alert("age is lies between  ") 
+ }
+else{
+  alert("age is not lies")
+}
+
+// //q2
+let age =prompt("what your age");
+console.log("you can" , age>18? "not drive":"drive")
+
+//q3
+let num=prompt("it is divisible");
+num =Number.parseInt(num)
+if(num%2==0 && num%3==0){
+  console.log("divsible by 2 and3 ")
+}
+else{
+  console.log(" not divisivble ")
+}
+
