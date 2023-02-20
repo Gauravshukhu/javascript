31f0b09fc50d575d7235d2e085d780afb7fbd63e
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-21 00:03:30 +0530

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,19 @@
+//string example 
+let car="supra"
+console.log(car.length)
+console.log(car[0])
+console.log(car)
+
+// template literals :- in this we use backtick ``
+let boy1="gaurav"
+let boy2="saurabh"
+let sentence=`${boy2} is a freind of ${boy1}`
+console.log(sentence)
+
+//escape sequence template 
+let gv = 'gau\nrav'
+console.log(gv)
+let gv1 = 'gau\trav'
+console.log(gv1.length)
+let gv2 = 'gau\rrav'
+console.log(gv2.length)
