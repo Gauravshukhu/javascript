63144f8981caa2c64109c32fa803d8483d7945b6
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-21 00:03:57 +0530

    Add files via upload

---
+++ properties of string and methods.js
@@ -0,0 +1,35 @@
+let name="gaurav"
+console.log(name.length)
+
+let jaadu="raju"
+console.log(jaadu.toUpperCase())
+
+
+
+
+
+
+
+let supercar="supra,ferrari"
+console.log(supercar.toLowerCase())
+
+
+let sexy1="mia khalifa "
+
+console.log(sexy1.slice(2))
+console.log(sexy1.replace("mia ", " rowdy "))
+
+let name1="jaadu"
+let friend="   raju  "
+
+// console.log(name.concat("is a name of",  friend , "ok" )
+console.log(friend)
+console.log(friend.trim())
+
+// let Name="gaurav"
+
+
+// for(i=0; i< object.key(Name).length; i++){
+//   console.log("here is my ", object.key(Name).length)
+// }
+  
