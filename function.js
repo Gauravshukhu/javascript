14ab9d43967a12fa027cf69a1a01907f041ec492
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-20 18:45:12 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,13 @@
+//function 
+
+function addition(x,y,){
+  console.log("Done")
+  return 1 + ( x+y ) / 2
+   
+}
+let a=2
+let b=8
+let c=7
+console.log("addition of a,b" ,  addition(a,b))
+console.log("addition of b,c" ,  addition(b,c))
+console.log("addition of a,c" ,  addition(a,c))
