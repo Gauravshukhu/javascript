da7d6fa588fe907b4a8ed5cfea037d5ceff048f5
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-20 18:44:18 +0530

    Add files via upload

---
+++ while and do while.js
@@ -0,0 +1,17 @@
+//while loop
+let n =prompt("enter the valuwe of n");
+n = Number.parseInt(n)
+let i=0;
+while(i<n){
+  console.log(i)
+  i++;
+ }
+
+//do while loop
+let n =prompt("enter the valuwe of n");
+n = Number.parseInt(n)
+let i=0;
+do{
+  console.log(i)
+  i++;
+}while(i<n)
