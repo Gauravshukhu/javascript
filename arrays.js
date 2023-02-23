0e1cb42254a9df975d767f4bb21183b35c6015ad
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-23 19:13:09 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,16 @@
+let class_12=[80,89,454,67]
+console.log(class_12)
+console.log(class_12[0]=2)//here i am change the value
+console.log(class_12[1])
+console.log(class_12[2])
+console.log(class_12[3])
+console.log(class_12.length)// here i got length of an array
+console.log(typeof class_12)
+
+//random quiz given by harry
+let class_123=[80,89,454,67]
+let i
+for(i=0; i<class_123.length;i++){
+  console.log(class_123[i])
+}
+
