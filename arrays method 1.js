63325d9e59aa2e0f9288ee746c8dedb4de8596ae
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-23 19:13:34 +0530

    Add files via upload

---
+++ arrays method 1.js
@@ -0,0 +1,56 @@
+//  n=[27,   26,   29,   23]  
+// let b=n.toString()
+// let c=n.join("-")//join mai hum kuch bhi join kra skte hai
+// let d=n.pop(29)//push
+// let e=n.shift(26)
+// let g=delete n[1]
+// console.log(b, typeof b)
+           
+//  console.log(c ,typeof c)               
+//  console.log(n,e)
+// console.log(n,g)
+
+
+//delete method in array
+// let num=[1,2,3,4,5,6,7,8,9,]
+// delete num[0]// here  delete are perform like operator
+// console.log(num)\
+
+
+//concat method in array
+// let gaurav=[1,222,34,56,45]
+// let saurabh=[24,34,567,78,90]
+// let sarvgya=[234,567,89,90,56]
+// let raju=gaurav.concat(saurabh,sarvgya )
+// let depu=sarvgya.concat(raju)
+// console.log(raju)
+
+//sort method in array 
+// let compare =(a,b)=>{ // this gemreally use for sorting in asscending order
+//   return a-b
+// }
+
+// let num=[ 45,356,45,789,099,4670]
+// num.sort(compare)
+        
+// console.log(num)
+
+//sort method in array 
+// let compare =(a,b)=>{ // this gemreally use for sorting in asscending order or descending order
+//    return a-b
+//  }
+
+
+// let num1=[ 45,356,45,789,099,4670]
+// let num2=[456,678,5676,879,3435]
+// num1.sort(compare)
+// num2.sort(compare)
+// console.log(num1)
+// console.log(num2)
+// num1.reverse()
+
+//splice and slice
+let num1=[ 45,356,45,789,099,4670]
+num1.splice(2,3,4,55,66)
+console.log(num1)
+  
