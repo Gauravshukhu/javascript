8bf4e897dcff4429f0fc2504a1136b91ad339ec1
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-24 17:47:25 +0530

    Add files via upload

---
+++ mapping practice set 5.js
@@ -0,0 +1,28 @@
+//q1
+// let arr=[5,7,8,9,10]
+// let b=prompt("enter the no")
+// a = Number.parseInt(arr)
+// arr.push(a)
+// console.log(arr)
+
+//q3 by reduce 
+// let arr=[20,30,40,50]
+// let b=arr.reduce((h1,h2)=>{
+//   return h1+h2/10
+// })
+// console.log(b)
+
+//q3 by filter
+// let arr=[20,30,41,42,97]
+// let b=arr.filter((x)=>{
+// return  x%10==0
+ 
+// })
+// console.log(b)
+
+//q4
+// let arr=[2,3,4,5,670]
+// let b=arr.map((value)=>{
+//   return value*value
+// })
+//   console.log(b)              
