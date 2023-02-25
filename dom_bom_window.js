e19cb8387ef6f50f72bc98f8c94c9870bf2ecee3
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-25 14:06:31 +0530

    Add files via upload

---
+++ dom_bom_window.js
@@ -0,0 +1,14 @@
+// you can se all the console into the browwer by using console .
+
+// window
+console.log(window)
+console.log(blur)
+
+//dom
+console.log(document)
+console.log(document.body)
+document.body.style.background="yellow"
+document.body.style.border=" 4rem black"
+
+//bom 
+location.href="https://filpkart.com"
