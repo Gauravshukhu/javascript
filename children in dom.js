2a6726feba455a46a25023e929c36213b4daba69
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-26 14:44:03 +0530

    Add files via upload

---
+++ children in dom.js
@@ -0,0 +1,12 @@
+//children-parent
+
+// console.log(document.body.firstChild)
+//  a=document.body.firstChild
+// console.log(a.parentNode)
+// console.log(a.firstChild.nextSibling)
+
+// navigation 
+
+let b=document.body
+console.log(" the first of b is: " ,b.firstChild )
+console.log(" the first element of b is: " ,b.firstElementChild )
