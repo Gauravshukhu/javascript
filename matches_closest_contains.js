fea50a12502912466ba41bc9159c1d46b8f72a93
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-27 00:12:38 +0530

    Add files via upload

---
+++ matches_closest_contains.js
@@ -0,0 +1,6 @@
+id1 =document.getElementById("id1")
+console.log(id1)
+console.log(id1.matches(".box"))
+console.log(id1.matches(".class"))
+console.log(id1.closest(".box"))
+console.log(id1.contains("id1"))
