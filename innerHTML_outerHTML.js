1e8bdc11e62d944c4e106183fcb520b42a46fa9b
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-28 00:47:17 +0530

    Add files via upload

---
+++ innerHTML_outerHTML.js
@@ -0,0 +1,18 @@
+// console.log(document.getElementsByTagName('span'))
+// console.dir(document.getElementsByTagName('span'))
+//  console.log(first.innerHTML)
+//  console.log(first.outerHTML)
+//    console.log(first.outerHtml="<div>hey i am strong</div>")
+
+
+document.body.firstChild
+" Hello world "
+//innerhtml only valid for elementnode,data or nodevalue
+document.body.firstChild.data
+// '\n  Hello world \n  '
+document.body.firstChild.nodeValue
+// '\n  Hello world \n  '
+
+
+// agar yaha hum textcontent likhenge toh toh body k andar jitna bhi textcontent hogab woh aajyaega
+console.log(document.body.textContent)
