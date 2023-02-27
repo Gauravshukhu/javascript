a5c43beafff26f38ec7854b8c8b272e0744e793a
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-02-28 00:47:42 +0530

    Add files via upload

---
+++ html_attribute.js
@@ -0,0 +1,11 @@
+let  a = Roe.getAttribute("class")
+console.log(a)
+
+//has attribute what kind of attribuite you have if u have attribute then its give true / false
+console.log(Roe.hasAttribute("class"))
+console.log(Roe.hasAttribute("id"))
+console.log(Roe.hasAttribute("style"))
+
+// Roe.setAttribute("hidden" , "True")
+Roe.removeAttribute("class")
+
