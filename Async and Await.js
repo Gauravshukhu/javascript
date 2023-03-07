382ff12489eff287d5792b3c6a6dc0ea78ae8286
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-03-07 16:29:34 +0530

    Add files via upload

---
+++ Async and Await.js
@@ -0,0 +1,26 @@
+async function gaurav(){
+	let delhiweather = new Promise((resolve ,reject)=>{
+		setTimeout(() =>{
+			resolve("27 Deg celius")
+		},2000)
+	})
+
+	let Bangloreweather = new Promise((resolve ,reject)=>{
+		setTimeout(() =>{
+			resolve("21")
+		},4000)
+	})
+
+	console.log("Fetching Delhi weather Please wait.... ")
+	let delhiW =await delhiweather
+	console.log("Fetching Delhi weather :"  + delhiW)
+
+	console.log("Fetching banglore weather Please wait.... ")
+	let BangloreW =await Bangloreweather
+	console.log("Fetching banglore weather :"  + BangloreW)
+	return[delhiW ,BangloreW]
+}
+let a = gaurav()
+a.then((value) => {
+	console.log(value)
+})
