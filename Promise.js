31b390738d9d77535be1a5cbdfd80ff5c6f29c2e
Gaurav shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-04-13 19:05:51 +0530

    Create Promise.js

---
+++ Promise.js
@@ -0,0 +1,21 @@
+let promise = new Promise(function(resolve, reject) {
+			  
+	alert("hello")
+	resolve(44)
+})
+console.log("hello i am gaurav1")
+setTimeout(function() {
+	console.log(" My name is " + " gaurav2 ")
+},2000)
+
+console.log("i am gaurav3")
+	console.log(promise)
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
