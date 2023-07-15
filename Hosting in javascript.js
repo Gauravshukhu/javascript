ab094b0fb38ca5e00c739cc8d1fc2157bb7f23f8
Gaurav Shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-07-16 00:04:37 +0530

    Add files via upload

---
+++ Hosting in javascript.js
@@ -0,0 +1,9 @@
+//folowing two lines runs succesfull due to javascript hosting 
+// var a = 9
+console.log(a)
+greet()
+function greet(){
+	console.log("good morning")
+}
+var a = 9 // declarations hosted into the top but intalization is not
+console.log(a)
