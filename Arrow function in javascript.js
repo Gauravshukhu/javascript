56a09bc06bd85277941a22953c359463ae544aa8
Gaurav Shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-07-16 16:00:31 +0530

    Add files via upload

---
+++ Arrow function in javascript.js
@@ -0,0 +1,24 @@
+const sayHello = name => {
+  console.log("greeting" + " " + name)
+	
+  console.log("hi")
+}
+
+
+
+const x = {
+  name: "Harry",
+  role: "Js Developer",
+  exp: 30,
+  show: function() {
+    //  let that = this
+    // // console.log(this)
+    setTimeout(() => {
+      console.log(`The name is ${this.name}\nThe role is ${this.role}`)
+    }, 2000)
+  }
+}
+
+console.log(x.name, x.exp)
+x.show()
+// sayHello("good morning", "hi")
