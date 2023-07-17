17c3cae8e0cf4b05553ff01920f408c5e1ce1f63
Gaurav Shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-07-18 00:43:58 +0530

    Add files via upload

---
+++ regular expression.js
@@ -0,0 +1,7 @@
+const regex1= /bad/g
+const text1 = " gaurav is bad bad very bad boy"
+console.log(text1.replace(regex1, "good"))
+
+const regex = /.arry/g
+const text = " gaurav is bad bad very bad boy larry parry"
+console.log(text.replace(regex, "good"))
