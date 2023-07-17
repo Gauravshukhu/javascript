1427b0e48924a0d821f982fa4bdce909da4928a0
Gaurav Shukla <100159334+Gauravshukhu@users.noreply.github.com>
2023-07-17 15:13:30 +0530

    Add files via upload

---
+++ module1.js
@@ -0,0 +1,10 @@
+const hello = () =>{
+console.log("heloo gaurav")
+}
+module.exports = hello;
+
+const ahello = (name) =>{
+    console.log(" Hello " + name)
+   
+}
+    module.exports ={hello, ahello};
