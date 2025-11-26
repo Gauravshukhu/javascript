bbe8f82f85227c8c3bc554d568ea0a074ba99d54
Gaurav Shukla <100159334+Gauravshukhu@users.noreply.github.com>
2025-11-26 20:28:00 +0530

    Update Cookies.js

--- Cookies.js
+++ Cookies.js
@@ -1,6 +1,6 @@
 console.log(document.cookie)
 document.cookie="name1=gaurav12234"
-document.cookie="name2=gaurav1223444"
+document.cookie="name2=gaurav12234444"
 let key=prompt("enter the key")
 let value=prompt("enter the value")
 document.cookie=`${encodeURIComponent(key)}=${encodeURIComponent(value)}`
